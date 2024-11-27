cfc49b1a2d11b26cfdb8f6af70d03c095cddc542
Martín Fernández <paziusss@gmail.com>
2024-11-28 00:22:44 +0100

    add autofrontlight-kindle, removed upstream

---
+++ autofrontlight-kindle.koplugin/_meta.lua
@@ -0,0 +1,5 @@
+return {
+    name = "autofrontlight-kindle",
+    fullname = "Auto frontlight",
+    description = "Automatically turns the frontlight on and off once brightness in the environment reaches a certain level.",
+}
