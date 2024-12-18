0529a9c2c02e1aeb4019212117c248a88d5838ed
Martín Fernández <975883+pazos@users.noreply.github.com>
2024-12-18 19:42:25 +0100

    add exporters removed upstream (#27)
    * flomo, memos
    
    Requires koreader/koreader#12641

---
+++ provider-old-exporters.koplugin/main.lua
@@ -0,0 +1,17 @@
+local Provider = require("provider")
+local WidgetContainer = require("ui/widget/container/widgetcontainer")
+
+local FlomoImpl = require("flomo")
+local MemosImpl = require("memos")
+
+local OldExporters = WidgetContainer:extend{
+    name = "unsupported-exporters",
+    is_doc_only = false,
+}
+
+function OldExporters:init()
+    Provider:register("exporter", "flomo", FlomoImpl)
+    Provider:register("exporter", "memos", MemosImpl)
+end
+
+return OldExporters
