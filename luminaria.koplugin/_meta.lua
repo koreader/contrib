707c9bf707c4f1547b30c7d5a1c82db72bf86e8b
jamesisonfire21-glitch <jamesisonfire21@gmail.com>
2026-03-24 21:36:01 +0000

    Add files via upload

---
+++ luminaria.koplugin/_meta.lua
@@ -0,0 +1,6 @@
+return {
+  name        = "Luminaria Sync",
+  fullname    = "Luminaria Highlights Sync",
+  description = "Export your KOReader highlights directly to your Luminaria website",
+  version     = 1,
+}
