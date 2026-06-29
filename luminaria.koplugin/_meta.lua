25ae08071e78c42e831b70c58732646f79c8b820
jamesisonfire21-glitch <jamesisonfire21@gmail.com>
2026-06-29 16:17:52 +0100

    luminaria.koplugin: convert to submodule (#146)
    * Remove luminaria.koplugin files, will re-add as submodule
    
    * Add luminaria.koplugin as a submodule, pinned to v1.1.1

--- luminaria.koplugin/_meta.lua
+++
@@ -1,6 +0,0 @@
-return {
-  name        = "luminaria",
-  fullname    = "Luminaria Highlights Sync",
-  description = "Export your KOReader highlights directly to your Luminaria website",
-  version     = 1,
-}
